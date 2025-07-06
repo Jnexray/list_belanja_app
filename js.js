e41ba15a85b64d4ec35b72fe5ef44efda91bb3ba
HO-IADS-ALVIN <jnesecond@gmail.com>
2025-07-06 18:55:17 +0700

    perubahan pada keterangan codingan

--- js.js
+++ js.js
@@ -21,11 +21,13 @@
         });
     }
 
+// addItem()  Mengambil isi dari input teks dan Jika kosong, fungsi berhenti
+  
     function addItem() {
       const input = document.getElementById('itemInput');
       const itemText = input.value.trim();
       if (itemText === '') return;
-
+  //Jika input valid, membuat elemen baru dan  Menambahkan ke daftar
       const list = document.getElementById('shoppingList');
       const li = document.createElement('li');
       li.className = 'list-group-item d-flex justify-content-between align-items-center';
@@ -33,7 +35,7 @@
       list.appendChild(li);
       input.value = '';
     }
-
+//Menghapus elemen dari DOM ketika tombol "Hapus" ditekan
     function removeItem(button) {
       button.parentElement.remove();
     }
